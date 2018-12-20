2c5a9f4c94de82462883081d3181c86c3f4719eb
Manaye <hiwotmanaye2@gmail.com>
2018-12-20 06:16:04 -0800

    add img

--- src/main/java/com/codefellowship/codefellowship/CodefellowshipController.java
+++ src/main/java/com/codefellowship/codefellowship/CodefellowshipController.java
@@ -1,48 +1,76 @@
 package com.codefellowship.codefellowship;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
 //import sun.security.util.Password;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @Controller
 public class CodefellowshipController {
-//index page
 
- @ResponseBody
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model m) {
-        return "index page";
-    }
-   //signup page
-
- @Autowired
- private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-@ResponseBody
+@Autowired
+private BCryptPasswordEncoder bCryptPasswordEncoder;
+private  ApplicationUserRepository userRepo;
+//index
+@RequestMapping(value = "/", method = RequestMethod.GET)
+public String index(Model m) { return "index"; }
+//signup page
 @RequestMapping(value = "/signup", method = RequestMethod.GET)
+public String signup(Model m) { return "signup";}
 
-public String signup(Model m) {
-//    bCryptPasswordEncoder.encode(password);
-    return "signup";}
+@RequestMapping(value = "/signup", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+        produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+public RedirectView create(Model m,
+        @RequestParam String firstname,
+        @RequestParam String lastname,
+        @RequestParam String dateofbirth,
+        @RequestParam String username,
+        @RequestParam String password,
+        @RequestParam String bio
+        ){
+password = bCryptPasswordEncoder.encode(password);
+System.out.println(password);
+SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+try{
 
-// @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    ApplicationUser newuser = new ApplicationUser(firstname,  lastname,sdf.parse(dateofbirth),  username, password, bio);
+    userRepo.save(newuser);
+    m.addAttribute("profiles",userRepo.findAll());
+    System.out.println(sdf.parse(dateofbirth));
 
+}
+catch (Exception e){
+    System.out.println(e.getMessage());
+}
 
+    return new RedirectView("profiles");
+
+}
+//profiles
+@RequestMapping(value = "/profiles", method = RequestMethod.GET)
+public String showProfiles(Model m) {
+//    m.addAttribute("profiles",userRepo.findAll());
+    return "profiles";
+}
 //
-//    ApplicationUser = new ApplicationUser
 //get a single user
-    @RequestMapping(value="/users/{id}")
-    @ResponseBody
-    public String getUser(@PathVariable("id") int id){
-        return "user id: "+id;
+    @RequestMapping(value="/users/{id}", method = RequestMethod.GET)
+    public String getUser(@PathVariable long id,Model m){
+ //Optional<ApplicationUser> user = userRepo.findById(id);
+   //  m.addAttribute("user", user.get());
+        return "user";
     }
 
 }
--- src/main/java/com/codefellowship/codefellowship/Security/WebSecurityConfig.java
+++ src/main/java/com/codefellowship/codefellowship/Security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.codefellowship.codefellowship.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,5 +44,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
                     .authorizeRequests()
                     .antMatchers("/*").permitAll();
         }
-
+//        @Override
+//        @Bean
+//        public AuthenticationManager authenticationManagerBean() throws Exception {
+//            return super.authenticationManagerBean();
+//        }
     }
